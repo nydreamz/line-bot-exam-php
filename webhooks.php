691f37aac97c596ce831b1b09cedb13438c51012
nydreamz <48614147+nydreamz@users.noreply.github.com>
2019-03-16 14:34:11 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -9,6 +9,7 @@ $idPush = 'U41dce961faea3f904c6377f122383327';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+print_r($bot);
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
 $response = $bot->pushMessage($idPush, $textMessageBuilder);
 
