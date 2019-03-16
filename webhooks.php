de16e38b003c095920abe5b9df1b59b320708d11
nydreamz <48614147+nydreamz@users.noreply.github.com>
2019-03-16 14:00:38 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,8 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'VrCVFLlZ2F1zQWf4RovbIi4ArY3CM977Cc22JyNBRUWQkVXpiz4xJkToFovIwFmkQiPjUPJI29IXnwg7B7oSU8NEcnsOnU6Yk5lH465fnm9z+OBcxfDb/SNhtMr8ryenSpAFdMpVAJRhNAU5n7kLHAdB04t89/1O/w1cDnyilFU=
+';
 
 // Get POST body content
 $content = file_get_contents('php://input');
