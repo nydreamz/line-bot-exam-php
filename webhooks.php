a373a082dd4924857c62dc146d7e1d544556136a
nydreamz <48614147+nydreamz@users.noreply.github.com>
2019-03-16 15:09:11 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -42,7 +42,8 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $text,
+				'xyz'  => 'abc',
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
