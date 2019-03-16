8fb5125b050ef78adaf00cee575157eb52cd9b76
nydreamz <48614147+nydreamz@users.noreply.github.com>
2019-03-16 15:45:06 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -72,7 +72,7 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		
+		$replyToken = "DOG";
 		if($event['message']['text'] == 'abc')
 			$replyToken = "XYZ";
 		
